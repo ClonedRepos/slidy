843aaf40aae94e1c69c3ef2dfddb9c25b0a1e8e4
Dianne M. Crummett <dcrummett@holosphere.gq>
2020-11-17 11:27:48 +0100

    T's slides

---
+++ Takahashi.md
@@ -0,0 +1,28 @@
+# Presentation Slides as md files !
+
+## outline format
+
+```
+This is a slide heading
+<tab>This is a top level text bullet
+<tab><tab>This is a second level text bullet
+This is another slide heading
+```
+
+In other words:
+
+### No tabs for a heading
+
+* One tab for a first level bullet point
+** Two tabs for a second level bullet point
+and so on
+
+## [Takahashi Method](https://en.wikipedia.org/wiki/Takahashi_method)
+
+see also [/trikita's slides]()
+
+
+### rendering
+
+ - Android : <http://trikita.co/slide/>
+   see also [trikita:slide] and [trikita:slide-html]
--- build.js
+++ build.js
@@ -3,12 +3,17 @@ var fs = require('fs');
 var uglify = require("uglify-js").minify;
 var cssmin = require('cssmin');
 
-var css = fs.readFileSync("src/slide.css", encoding='utf8');
+var style = fs.readFileSync("src/style.css", encoding='utf8');
 var js = fs.readFileSync("src/slide.js", encoding='utf8');
 var html = fs.readFileSync("src/slide.html", encoding='utf8');
 
+var css = fs.readFileSync("src/slides.css", encoding='utf8');
+var slides = fs.readFileSync("src/slides.md", encoding='utf8');
+
 var inline = html
   .replace(/<script src="slide.js"><\/script>/, '<script>'+uglify(js, {fromString: true}).code+'</script>')
-  .replace(/<link .*href="slide.css">/, '<style>'+cssmin(css)+'</style>');
+  .replace(/<link .*href="style.css">/, '<style>'+cssmin(style)+'</style>')
+  .replace(/<style.*>.*slides.css[^<]*<\/style>/, '<script media="screen">\n'+cssmin(css)+'</style>')
+  .replace(/<pre id="slide">[^<]*<\/pre>/m, '<pre>\n'+slides+'</pre>');
 
 console.log(inline);
